7938a653efb2c1b39a911cf43a10195261742702
Sandro Costa Gomes <01743841060>
2024-07-03 11:30:03 -0300

    implementa correcoes

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-var matriz = [[0, 0, 0, 0, 0, 0, 0],
+var matriz = [[1, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0],
               [0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0]];
+              [0, 0, 0, 0, 0, 0, 1]];
 
 function tiro(id){
+    document.getElementById(id).disabled = true;
+    document.getElementById(id).style.transform = "none";
     let linha = parseInt(id.charAt(0))-1;
     let coluna = parseInt(id.charAt(1))-1;
     if(matriz[linha][coluna] != 1){
-        document.getElementById(id).disabled = true;
+        
         document.getElementById(id).style.backgroundColor = "#d1d1d1";
-        document.getElementById(id).style.transform = "none";
+        
 
     }
 
