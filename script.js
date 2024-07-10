b4597e516296b7af6897874b8c27c1955a469026
Sandro Costa Gomes <01743841060>
2024-07-10 11:44:25 -0300

    implementa tentativa de random

--- script.js
+++ script.js
@@ -8,11 +8,23 @@ let matriz = [
 [0, 0, 0, 0, 0, 0, 0]
 ];
 
-l
-
 function iniciaJogo() {
-    
+    var linha = Math.random()*5;
+    var coluna = Math.random()*7;
+    if (matriz[linha][coluna] == 0 && matriz[linha+1][coluna+1] == 0 && matriz[linha+2][coluna +2] == 0){
+        for (i=0; i<3; i++){
+            matriz[linha+i][coluna+i] = 1;
+        }
+    }
+}
 
+function alert(){
+    for(i=0; i<7; i++){
+        for(j=0; i<7; i++){
+            document.alert(matriz[i][j]);
+        }
+    }
+    
 }
 
 function tentantivaTiro(id) {
@@ -28,7 +40,5 @@ function tentantivaTiro(id) {
     else {
         img.src = "/img/marine-hit.png";
     }
-    matriz[linha][coluna] = 1;
-    //alert(matriz);
 }
 
