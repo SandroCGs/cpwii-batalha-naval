ccb230698e5b1dfbd78e6a623753c467492ca3ef
Sandro Costa Gomes <01743841060>
2024-07-23 11:30:51 -0300

    implementa variacao de barcos

--- script.js
+++ script.js
@@ -34,93 +34,168 @@ function iniciaJogo() {
             }
         }
     }
-    
-    var barcos = 0;
-    while(barcos < 3){
+
+    var barcos = 1;
+    while(barcos < 4){
+        
         var coluna = Math.floor(Math.random() * 5);
         var linha = Math.floor(Math.random() * 7);
+
         if (Number(matriz[linha][coluna]) == 0 && Number(matriz[linha][coluna+1]) == 0 && Number(matriz[linha][coluna+2]) == 0){
             for (i=0; i<3; i++){
-                matriz[linha][coluna+i] = 1;
+                matriz[linha][coluna+i] = barcos;
             }
             barcos++;
         }
+        
+        
+        for(i=0; i<7; i++){
+            for(j=0; j<7; j++){
+                console.log(matriz[i][j]);
+            }
+        }   
+        
     }
+
 }
 
-function alerta(){
-    for(i=0; i<7; i++){
-        for(j=0; j<7; j++){
-            console.log(matriz[i][j]);
-        }
-    }
-    
-}
 
-function mudaImagemEsq(idEsquerda){
+function mudaImagemEsq(idEsquerda, barco){
     var buttonEsq = document.getElementById(idEsquerda);
     var img = buttonEsq.querySelector('img');
-    img.src = "./img/navio-esquerda.png";
+    var contador = 1;
+    img.src = "./img/navio-esquerda" + barco + ".png";
+    contador++;
 }
 
-function mudaImagemMei(idMeio){
+function mudaImagemMei(idMeio, barco){
     var buttonMei = document.getElementById(idMeio);
     var img = buttonMei.querySelector('img');
-    img.src = "./img/navio-meio.png";
+    img.src = "./img/navio-meio"+ barco + ".png";
 }
 
-function mudaImagemDir(idDireita){
+function mudaImagemDir(idDireita, barco){
     var buttonDir = document.getElementById(idDireita);
     var img = buttonDir.querySelector('img');
-    img.src = "./img/navio-direita.png";
+    img.src = "./img/navio-direita" + barco + ".png";
 }
-
 
 function tentantivaTiro(id) {
     document.getElementById(id).disabled = true;
     document.getElementById(id).style.transform = "none";
     
-        let linha = parseInt(id.charAt(0)) - 1;
-        let coluna = parseInt(id.charAt(1)) - 1;
-        var button = document.getElementById(id);
-        var img = button.querySelector('img');
-        if (matriz[linha][coluna] != 1) {
-            img.src = "./img/marine-miss.png";
-            tentativa--;
-        }
-
-        if (matriz[linha][coluna] == 1){
-            matriz[linha][coluna] = 2;
-            if (matriz[linha][coluna] == 2 && matriz[linha][coluna+1] == 2 && matriz[linha][coluna+2] == 2){
-                let idb = parseInt(id)+1;
-                let idc = parseInt(id)+2;
-                console.log(id + " + " + idb + " + " + idc)
-                mudaImagemEsq(id);
-                mudaImagemMei(idb);
-                mudaImagemDir(idc);
-            }
-            else if (matriz[linha][coluna+1] == 2 && matriz[linha][coluna-1] == 2){
-                let ida = parseInt(id)-1;
-                let idc = parseInt(id)+1;
-                console.log( ida + " + " + id + " + " + idc)
-                mudaImagemEsq(ida);
-                mudaImagemMei(id);
-                mudaImagemDir(idc);
-            }
-            else if (matriz[linha][coluna-1] == 2 && matriz[linha][coluna-2] == 2){
-                let idb = parseInt(id)-1;
-                let ida = parseInt(id)-2;
-                console.log( ida + " + " + idb + " + " + id)
-                mudaImagemEsq(ida);
-                mudaImagemMei(idb);
-                mudaImagemDir(id);
-            }
-            else {
-                img.src = "./img/marine-hit.png";            
-            }
-        }
-
+    let linha = parseInt(id.charAt(0)) - 1;
+    let coluna = parseInt(id.charAt(1)) - 1;
+    var button = document.getElementById(id);
+    var img = button.querySelector('img');
+    if (matriz[linha][coluna] == 0) {
+        img.src = "./img/marine-miss.png";
+        tentativa--;
     }
+
+    if (matriz[linha][coluna] == 1){
+        matriz[linha][coluna] = "a";
+        if (matriz[linha][coluna] == "a" && matriz[linha][coluna+1] == "a" && matriz[linha][coluna+2] == "a"){
+            let idb = parseInt(id)+1;
+            let idc = parseInt(id)+2;
+            console.log(id + " + " + idb + " + " + idc)
+            mudaImagemEsq(id, 1);
+            mudaImagemMei(idb, 1);
+            mudaImagemDir(idc, 1);
+        }
+        else if (matriz[linha][coluna+1] == "a" && matriz[linha][coluna-1] == "a"){
+            let ida = parseInt(id)-1;
+            let idc = parseInt(id)+1;
+            console.log( ida + " + " + id + " + " + idc)
+            mudaImagemEsq(ida, 1);
+            mudaImagemMei(id, 1);
+            mudaImagemDir(idc, 1);
+        }
+        else if (matriz[linha][coluna-1] == "a" && matriz[linha][coluna-2] == "a"){
+            let idb = parseInt(id)-1;
+            let ida = parseInt(id)-2;
+            console.log( ida + " + " + idb + " + " + id)
+            mudaImagemEsq(ida, 1);
+            mudaImagemMei(idb, 1);
+            mudaImagemDir(id, 1);
+        }
+        else {
+            img.src = "./img/marine-hit.png";            
+        }
+    }
+
+    if (matriz[linha][coluna] == 2){
+        matriz[linha][coluna] = "b";
+        if (matriz[linha][coluna] == "b" && matriz[linha][coluna+1] == "b" && matriz[linha][coluna+2] == "b"){
+            let idb = parseInt(id)+1;
+            let idc = parseInt(id)+2;
+            console.log(id + " + " + idb + " + " + idc)
+            mudaImagemEsq(id, 2);
+            mudaImagemMei(idb, 2);
+            mudaImagemDir(idc, 2);
+        }
+        else if (matriz[linha][coluna+1] == "b" && matriz[linha][coluna-1] == "b"){
+            let ida = parseInt(id)-1;
+            let idc = parseInt(id)+1;
+            console.log( ida + " + " + id + " + " + idc)
+            mudaImagemEsq(ida, 2);
+            mudaImagemMei(id, 2);
+            mudaImagemDir(idc, 2);
+        }
+        else if (matriz[linha][coluna-1] == "b" && matriz[linha][coluna-2] == "b"){
+            let idb = parseInt(id)-1;
+            let ida = parseInt(id)-2;
+            console.log( ida + " + " + idb + " + " + id)
+            mudaImagemEsq(ida, 2);
+            mudaImagemMei(idb, 2);
+            mudaImagemDir(id, 2);
+        }
+        else {
+            img.src = "./img/marine-hit.png";            
+        }
+    }
+
+    if (matriz[linha][coluna] == 3){
+        matriz[linha][coluna] = "c";
+        if (matriz[linha][coluna] == "c" && matriz[linha][coluna+1] == "c" && matriz[linha][coluna+2] == "c"){
+            let idb = parseInt(id)+1;
+            let idc = parseInt(id)+2;
+            console.log(id + " + " + idb + " + " + idc)
+            mudaImagemEsq(id, 3);
+            mudaImagemMei(idb, 3);
+            mudaImagemDir(idc, 3);
+        }
+        else if (matriz[linha][coluna+1] == "c" && matriz[linha][coluna-1] == "c"){
+            let ida = parseInt(id)-1;
+            let idc = parseInt(id)+1;
+            console.log( ida + " + " + id + " + " + idc)
+            mudaImagemEsq(ida, 3);
+            mudaImagemMei(id, 3);
+            mudaImagemDir(idc), 3;
+        }
+        else if (matriz[linha][coluna-1] == "c" && matriz[linha][coluna-2] == "c"){
+            let idb = parseInt(id)-1;
+            let ida = parseInt(id)-2;
+            console.log( ida + " + " + idb + " + " + id)
+            mudaImagemEsq(ida, 3);
+            mudaImagemMei(idb, 3);
+            mudaImagemDir(id, 3);
+        }
+        else {
+            img.src = "./img/marine-hit.png";            
+        }
+    
+    }
+    function alerta(){
+        for(i=0; i<7; i++){
+            for(j=0; j<7; j++){
+                console.log(matriz[i][j]);
+            }
+        }   
+    }
+}
+
+
 
 
 
